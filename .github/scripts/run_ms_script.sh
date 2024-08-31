30af7ec8224d375cbfc2540826e8d3ddb4118743
Lionel Teo <93119265+imda-lionelteo@users.noreply.github.com>
2024-08-31 16:47:25 +0800

    update

---
+++ .github/scripts/run_ms_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update endpoints based on the provided list
+echo "Updating endpoints..."
+# IFS=';' read -ra ENDPOINTS <<< "$MOONSHOT_UPDATE_ENDPOINTS_LIST"
+# for cmd in "${ENDPOINTS[@]}"; do
+#   echo "Executing: $cmd"
+#   eval $cmd
+# done
+
+# # Trigger the cookbook run, which may take some time
+# echo "Triggering cookbook run... This may take some time."
+# IFS=';' read -ra COOKBOOKS <<< "$MOONSHOT_RUN_COOKBOOKS_LIST"
+# for cmd in "${COOKBOOKS[@]}"; do
+#   echo "Executing: $cmd"
+#   eval $cmd
+# done
+
+echo "Run completed"
