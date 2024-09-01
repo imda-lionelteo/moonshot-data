78e08175240e25d252b1b97855a9e4ce35d5c4ec
Lionel Teo <93119265+imda-lionelteo@users.noreply.github.com>
2024-09-01 10:51:54 +0800

    push

--- .github/scripts/run_ms_script.sh
+++ .github/scripts/run_ms_script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Function to replace placeholders with actual environment variable values
+replace_placeholders() {
+  local cmd=$1
+  cmd=${cmd//\$GITHUB_RUN_ID/$GITHUB_RUN_ID}
+  echo "$cmd"
+}
+
 # Update endpoints based on the provided list
 echo "Updating endpoints..."
 IFS=';' read -ra ENDPOINTS <<< $MOONSHOT_UPDATE_ENDPOINTS_LIST
@@ -12,6 +19,7 @@ done
 echo "Triggering cookbook run... This may take some time."
 IFS=';' read -ra COOKBOOKS <<< $MOONSHOT_RUN_COOKBOOKS_LIST
 for cmd in "${COOKBOOKS[@]}"; do
+  cmd=$(replace_placeholders "$cmd")
   echo "Executing: $cmd"
   eval "$cmd"
 done
