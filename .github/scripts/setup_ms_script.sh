c5484bbd9d1d17f6ad68f968bc4f73a983760cf2
Lionel Teo <93119265+imda-lionelteo@users.noreply.github.com>
2024-09-03 00:32:05 +0800

    test

--- .github/scripts/setup_ms_script.sh
+++ .github/scripts/setup_ms_script.sh
@@ -6,8 +6,9 @@ pip install "aiverify-moonshot[all]"
 
 # Clone the forked repository and install its requirements
 echo "Cloning forked repository and installing requirements..."
-git clone https://github.com/$GITHUB_MOONSHOT_CI_REPO_LOC
-pip install -r moonshot-data/requirements.txt
+ls -la
+# git clone https://github.com/$GITHUB_MOONSHOT_CI_REPO_LOC
+# pip install -r moonshot-data/requirements.txt
 
 # Set logging information
 echo "Setting log info..."
