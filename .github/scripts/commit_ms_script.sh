46a21c3e5794e6766aec85c11ed27d465013cc87
Lionel Teo <93119265+imda-lionelteo@users.noreply.github.com>
2024-09-03 00:49:55 +0800

    nn

--- .github/scripts/commit_ms_script.sh
+++ .github/scripts/commit_ms_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Change directory to moonshot-data
-cd moonshot-data
 # Configure git user for committing changes
 git config --global user.email "ci@example.com"
 git config --global user.name "CI Bot"
@@ -10,4 +8,4 @@ git add -f ./generated-outputs/*
 # Commit the changes with a message including the pipeline ID
 git commit -m "Update generated files [ci skip] - Pipeline ID ($GITHUB_RUN_ID)"
 # Push the changes to the repository, skipping CI for this push
-# git push -o ci.skip
+git push -o ci.skip
